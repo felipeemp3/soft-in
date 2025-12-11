272a4659c4a961798cf3a091c6f596a238090ab0
felipeemp3 <andresfelipebyt@hotmail.com>
2025-12-11 14:50:00 -0500

    conexion actualizada

--- src/conexion.php
+++ src/conexion.php
@@ -11,6 +11,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("La conexión falló: " . $conn->connect_error);
 }
-//  echo "conexion exitosa con MySQLi";
-
+// Opcional: Establecer charset
+$conn->set_charset("utf8mb4"); 
 ?>
